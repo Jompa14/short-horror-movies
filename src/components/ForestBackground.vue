54a4892d9ee23f5bba005d91464342720877ab2a
Jompa14 <joao.buchholz@gmail.com>
2023-07-09 22:21:51 -0300

    incial commit

---
+++ src/components/ForestBackground.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="forest-container"></div>
+</template>
+
+<script>
+import * as THREE from 'three';
+
+export default {
+    mounted() {
+        // Initialize the Three.js scene
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('forest-container').appendChild(renderer.domElement);
+
+        // Create and add your Three.js objects and animations here
+        // ...
+
+        // Start the animation loop
+        function animate() {
+            requestAnimationFrame(animate);
+            // Update objects, camera, etc. here
+            // ...
+            renderer.render(scene, camera);
+        }
+        animate();
+    }
+}
+</script>
+
+<style scoped>
+#forest-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
