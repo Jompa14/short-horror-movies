f83acf97a78504e8b9e2ee76d34573fa27cc7339
Jompa14 <joao.buchholz@gmail.com>
2023-07-13 17:07:30 -0300

    small changes

--- src/components/ForestBackground.vue
+++ src/components/ForestBackground.vue
@@ -1,106 +1,104 @@
 <template>
-  <div id="forest-container"></div>
+    <div id="forest-container"></div>
 </template>
 
 <script>
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default {
-  mounted() {
-    // Initialize the Three.js scene
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('forest-container').appendChild(renderer.domElement);
+    mounted() {
+        // Initialize the Three.js scene
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            100
+        );
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document
+            .getElementById("forest-container")
+            .appendChild(renderer.domElement);
 
-    // Create a blue moonlight
-    const moonLight = new THREE.PointLight(0x6336e3, 1, 40);
-    moonLight.position.set(2, 3, 7);
-    scene.add(moonLight);
-    // const sphereSize = 1;
-    // const pointLightHelper = new THREE.PointLightHelper(moonLight, sphereSize);
-    // scene.add(pointLightHelper);
+        // texture loader
+        const textureLoader = new THREE.TextureLoader();
 
-    // Create custom tree geometry
-    const trunkGeometry = new THREE.CylinderGeometry(0.2, 0.4, 5, 8);
-    const foliageGeometry = new THREE.SphereGeometry(3, 8, 6);
+        // Load ground texture for the floor
+        const groundTexture = textureLoader.load(require("../textures/ground.jpg"));
+        groundTexture.wrapS = THREE.RepeatWrapping;
+        groundTexture.wrapT = THREE.RepeatWrapping;
+        groundTexture.repeat.set(20, 20);
+        const groundSize = 200;
+        // floor
+        const floorGeometry = new THREE.PlaneGeometry(groundSize, groundSize);
+        const floorMaterial = new THREE.MeshLambertMaterial({
+            map: groundTexture
+        });
+        const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+        floorMesh.receiveShadow = true;
+        floorMesh.rotation.x = -Math.PI / 2;
+        scene.add(floorMesh);
 
-    // Load textures for trunk and foliage
-    const textureLoader = new THREE.TextureLoader();
-    const trunkTexture = textureLoader.load(require('../textures/trunk.jpg'));
-    const foliageTexture = textureLoader.load(require('../textures/leaves.jpg'));
+        // Create trunk
+        const trunkTexture = textureLoader.load(require("../textures/trunk.jpg"));
+        const trunkMaterial = new THREE.MeshLambertMaterial({
+            map: trunkTexture
+        });
+        const trunkGeometry = new THREE.CylinderGeometry(0.2, 0.4, 5, 8);
+        const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
+        trunk.position.y = 1;
 
-    const trunkMaterial = new THREE.MeshLambertMaterial({ map: trunkTexture });
-    const foliageMaterial = new THREE.MeshLambertMaterial({ color: 0x478c74, map: foliageTexture });
-    foliageMaterial.map.repeat.set(2, 1.5);
+        // Create foliage
+        const foliageTexture = textureLoader.load(require("../textures/leaves.jpg"));
+        const foliageGeometry = new THREE.SphereGeometry(3, 8, 6);
+        const foliageMaterial = new THREE.MeshLambertMaterial({
+            color: 0x478c74,
+            map: foliageTexture
+        });
+        foliageMaterial.map.repeat.set(2, 1.5);
+        const foliage = new THREE.Mesh(foliageGeometry, foliageMaterial);
+        foliage.position.y = 6.5;
 
-    // Load ground texture for the floor
-const groundTexture = textureLoader.load(require('../textures/ground.jpg'));
-groundTexture.wrapS = THREE.RepeatWrapping;
-groundTexture.wrapT = THREE.RepeatWrapping;
-groundTexture.repeat.set(20, 20);
+        
+        // Create custom tree geometry
+        const tree = new THREE.Object3D();
+        tree.add(foliage);
+        tree.add(trunk);
+        tree.position.set(6, 0, 0)
+        scene.add(tree);
+        
+        // Create a blue moonlight
+        const purpleLight = new THREE.PointLight(0x6336e3, 1, 40);
+        purpleLight.position.set(5, 2, 10);
+        scene.add(purpleLight);
+        
+        // Set up camera position
+        camera.position.set(0, 7, 15); // Position the camera to have an overview of the scene
 
-const groundSize = 200;
-const floorGeometry = new THREE.PlaneGeometry(groundSize, groundSize);
-const floorMaterial = new THREE.MeshLambertMaterial({ map: groundTexture });
-const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-floorMesh.receiveShadow = true;
-floorMesh.rotation.x = -Math.PI / 2;
-scene.add(floorMesh);
+        // Add camera controls
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.05;
 
-// Randomly position trees on the ground
-const numTrees = 300;
-const treePlacementRange = groundSize / 2;
-for (let i = 0; i < numTrees; i++) {
-  const tree = new THREE.Object3D();
-
-  // Create trunk
-  const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
-  trunk.position.y = 1;
-  tree.add(trunk);
-
-  // Create foliage
-  const foliage = new THREE.Mesh(foliageGeometry, foliageMaterial);
-  foliage.position.y = 6.5;
-  tree.add(foliage);
-
-  // Randomly position the tree on the ground
-  const posX = Math.random() * treePlacementRange * 2 - treePlacementRange;
-  const posZ = Math.random() * treePlacementRange * 2 - treePlacementRange;
-  tree.position.set(posX, 0, posZ);
-
-  scene.add(tree);
-}
-
-
-    // Set up camera position
-    camera.position.set(0, 10, 20); // Position the camera to have an overview of the scene
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    // Add camera controls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.05;
-
-    // Start the animation loop
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      controls.update(); // Update camera controls
+        // Start the animation loop
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+            controls.update(); // Update camera controls
+        }
+        animate();
     }
-    animate();
-  },
 };
 </script>
 
 <style scoped>
 #forest-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
